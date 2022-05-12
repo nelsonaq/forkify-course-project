f90949327089e6cac664ea67d6c1d4f44a60c123
Nelson <94856406+nelsonaq@users.noreply.github.com>
2022-05-13 01:45:45 +0800

    added and implemented fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './View.js';
 // import icons from '../img/icons.svg'; //* Parcel 1
 import icons from 'url:../../img/icons.svg'; //* Parcel 2
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -126,7 +127,9 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
+        <div class="recipe__quantity">${
+          fracty(ing.quantity) ? fracty(ing.quantity) : ''
+        }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit ?? ''}</span>
           ${ing.description}
